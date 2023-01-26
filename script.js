38f8c8576a1312b7658e350e96ed6c6ee9200864
Shubham Mali <shubhammali31299@gmail.com>
2023-01-26 23:27:29 +0530

    almost done

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector(".cross").style.display = "none";
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector(".sideBar").classList.toggle("sideBarGo");
+  if (document.querySelector(".sideBar").classList.contains("sideBarGo")) {
+    document.querySelector(".ham").style.display = "inline";
+    document.querySelector(".cross").style.display = "none";
+  } else {
+    document.querySelector(".ham").style.display = "none";
+    setTimeout(() => {
+      document.querySelector(".cross").style.display = "inline";
+    }, 300);
+  }
+});
+document.querySelector(".ham").addEventListener("click", () => {
+  document.querySelector("body").style.overflowY = "hidden";
+});
+document.querySelector(".cross").addEventListener("click", () => {
+  document.querySelector("body").style.overflowY = "visible";
+});
